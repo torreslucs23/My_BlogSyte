4e31577d9ce50f6f9f3e00c742f657fee18a52e3
torreslucs23 <luquinhastorres231002@gmail.com>
2022-10-13 14:22:19 -0300

    Create my blog and added some static files and templates

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+
+
+urlpatterns = [
+    path("", views.starting_page, name="initial-page"),
+    path("posts", views.posts, name = "posts-page"),
+    path("posts/<slug:slug>", views.post_detail, name = "post-detail-page")
+
+]
