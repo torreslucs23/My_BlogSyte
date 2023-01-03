3fd187fa4388fcf2bd650fd379ee30a4c4d90693
torreslucs23 <luquinhastorres231002@gmail.com>
2023-01-03 10:41:49 -0300

    added imagefield in the database

--- blog/urls.py
+++ blog/urls.py
@@ -3,9 +3,11 @@ from . import views
 
 
 
+
+
 urlpatterns = [
     path("", views.starting_page, name="initial-page"),
     path("posts", views.posts, name = "posts-page"),
     path("posts/<slug:slug>", views.post_detail, name = "post-detail-page")
 
-]
+] 
--- my_site/settings.py
+++ my_site/settings.py
@@ -130,3 +130,7 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+MEDIA_ROOT = BASE_DIR / "uploads"
+
+MEDIA_URL = "/posts/"
+
